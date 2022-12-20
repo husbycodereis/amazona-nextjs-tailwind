08937d07614f6700ead7a1338fd92829e27d6b49
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-12-20 15:25:58 +0300

    add to cart feature implemented

--- components/Layout.js
+++ components/Layout.js
@@ -1,34 +1,48 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
+import React, { useContext } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Store } from "../utils/Store";
 
-function Layout({title, children}) {
+function Layout({ title, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
   return (
     <>
-    <div className='flex flex-col justify-between min-h-screen'>
+      <div className="flex flex-col justify-between min-h-screen">
         <Head>
-        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
-        <meta name="description" content="Ecommerce Website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+          <title>{title ? title + " - Amazona" : "Amazona"}</title>
+          <meta name="description" content="Ecommerce Website" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <header>
-            <nav className='flex items-center justify-between h-12 px-4 shadow-md'>
-                <Link href = "/" className='text-lg font-bold'>
-                    Amazona
-                </Link>
-                <div>
-                    <Link href ="/cart" className='p-2'>Cart</Link>
-                    <Link href ="/login" className='p-2'>Login</Link>
-                </div>
-            </nav>
+          <nav className="flex items-center justify-between h-12 px-4 shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              Amazona
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart{" "}
+                {cart.cartItems.length > 0 && (
+                  <span className="px-2 py-1 ml-1 text-xs font-bold text-white bg-red-600 rounded-full ">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
         </header>
         {/* container makes the html element responsive based on the screen size */}
-        <main className='container px-4 m-auto mt-4'>{children}</main>
-        <footer className='flex items-center justify-center h-10 shadow-inner'>Copyright © 2022 Amazona</footer>
-    </div>
+        <main className="container px-4 m-auto mt-4">{children}</main>
+        <footer className="flex items-center justify-center h-10 shadow-inner">
+          Copyright © 2022 Amazona
+        </footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
